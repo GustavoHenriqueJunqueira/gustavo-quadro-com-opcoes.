deec6aee237ba29203cba8887e749f4488c4e28e
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-09-18 19:02:22 -0300

    teste

---
+++ Pasta Sem Título/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$nome = "Gustavo"; //O sinal de igual é um operador de atribuição,neste caso esta atribuindo o nome gustavo na variavel nome. 
+
+echo $nome . "mais alguma coisa"; //O sinal do ponto é o sinal de concatenação, será exibido o nome gustavo mais o restante do "mais alguma coisa"
+
+echo "<br/>";
+
+$nome .=" treinamento"; //Desta forma será exibido o nome antes declarado mais o a parala treinamento.
+
+echo $nome;
+
+?>
